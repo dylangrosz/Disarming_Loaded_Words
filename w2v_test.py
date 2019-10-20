1d74a4327d0f773c0653e46f33190ee7b6ec1be7
dylangrosz <dylangrosz@gmail.com>
2019-10-19 20:49:49 -0700

    added single gender scoring + modularization

--- w2v_test.py
+++ w2v_test.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import normalize
 from literal_tool import feminine_coded_words, masculine_coded_words
 import seaborn as sns
 
-model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=500000)
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=1000000)
 #model = api.load('word2vec-google-news-300')
 
 female_list = ["she", "her", "woman", "Mary", "herself", "daughter", "mother", "gal", "girl", "female"]
@@ -20,6 +20,8 @@ feminine_coded_words_unstemmed = [
     "affectionate",
     "bossy",
     "child",
+    "green",
+    "electrical",
     "cheer",
     "collaborative",
     "committed",
@@ -144,6 +146,15 @@ def build_gender_direction(model, comp = 1):
     #print(pca.explained_variance_ratio_)
     return pcs, pca.explained_variance_ratio_
 
+def build_single_gender_directions(model, comp = 1):
+    g_m, g_f = build_male_subspace(model), build_female_subspace(model)
+    pca_m, pca_f = PCA(n_components = comp), PCA(n_components = comp)
+    pcs_m, pcs_f = pca_m.fit_transform(g_m).squeeze(), \
+                   pca_f.fit_transform(g_f).squeeze()
+    print(pca_m.explained_variance_ratio_)
+    print(pca_f.explained_variance_ratio_)
+    return pcs_m, pca_m.explained_variance_ratio_, \
+           pcs_f, pca_f.explained_variance_ratio_
 
 def absolute_gender_score(word_a, word_b, model, delta = 1, comp = 1):
     v_a, v_b = model.wv[word_a], model.wv[word_b]
@@ -174,44 +185,118 @@ def relative_gender_score(word_a, word_b, base_score, model, delta = 1, comp = 1
     _, score = absolute_gender_score(word_a, word_b, model, delta = delta, comp = comp)
     return score / base_score
 
-if __name__ == '__main__':
-    g_d = build_gender_direction(model, comp = 1)
-    p1, p2 = "father", "doctor"
-    n1 = "mother"
-    print("{} is to {} as {} is to: ".format(p1, p2, n1))
-    #pp.pprint(model.most_similar(positive=[p1, n1], negative=[p2]))
+def build_male_subspace(model):
+    male_subspace = np.zeros((model.wv['news'].shape[0],0))
+    for w_m in male_list:
+        v_m = np.expand_dims(model.wv[w_m], axis=1)
+        male_subspace = np.concatenate((male_subspace, v_m), axis=1)
+    return male_subspace
 
+def build_female_subspace(model):
+    female_subspace = np.zeros((model.wv['news'].shape[0],0))
+    for w_f in female_list:
+        v_f = np.expand_dims(model.wv[w_f], axis=1)
+        female_subspace = np.concatenate((female_subspace, v_f), axis=1)
+    return female_subspace
+
+def absolute_single_gender_score(word, model, delta = 1, comp = 1):
+    v = model.wv[word]
+    g_m, weights_m, g_f, weights_f = build_single_gender_directions(model, comp = comp)
+    norm = np.linalg.norm(v)
+    #print("Norm: {}".format(norm_d))
+    if True or norm <= delta:
+        gp_m, gp_factor_m = gender_proj(v, g_m, model, weights = weights_m)
+        gp_f, gp_factor_f = gender_proj(v, g_f, model, weights = weights_m)
+        return (gp_m, gp_factor_m) if gp_factor_m > gp_factor_f else (gp_f, -1 * gp_factor_f)
+    else:
+        return 0 # are not semantically similar relative to delta
+def relative_single_gender_score(word, base_score, model, delta = 1, comp = 1):
+    _, score = absolute_single_gender_score(word, model, delta = delta, comp = comp)
+    return score / base_score if score > 0 else -1 * (score / base_score)
+
+def get_base_score(male_list, female_list):
     base_scores = []
     for v_f, v_m in zip(female_list, male_list):
         base_scores.append(absolute_gender_score(v_m, v_f, model, comp = 1)[1])
-    print(male_list)
-    print(female_list)
-    print(base_scores)
-    base_score = max(base_scores)
+    return max(base_scores)
 
-    word_m_bias, word_f_bias = "doctor", "nurse"
-    score = absolute_gender_score(word_m_bias, word_f_bias, model, comp = 1)[1]
-    print("Relative gender score of {} vs. {}: {} / {} = {}".format(word_m_bias, word_f_bias,
-                                                                    score, base_score,
-                                                                    relative_gender_score(word_m_bias, word_f_bias, base_score, model, comp = 1)))
-    w_concs = []
+def get_single_base_score(gender_list, isFemale=True):
+    scores = []
+    for w in gender_list:
+        scores.append(absolute_single_gender_score(w, model, comp=1)[1])
+    # print(isFemale)
+    # print(scores)
+    return min(scores) if isFemale else max(scores)
+
+def plot_single_bias(model, gender_list, coded_words_unstemmed,
+                     isFemale=True, TOP_N = 25, comp = 1, fn_name="bias.png"):
     scores_rel = []
+    base_score = get_single_base_score(gender_list, isFemale=isFemale)
+    for w in coded_words_unstemmed:
+        if w in model:
+            scores_rel.append((w,
+                               relative_single_gender_score(w, base_score, model, comp = comp)))
+    scores_rel.sort(reverse=(not isFemale), key=lambda x: x[1])
+    w_concs = [w for w, s in scores_rel]
+    scores_rel_s = [s for w, s in scores_rel]
+    plt.figure(figsize=(14, 7))
+    bp = sns.barplot(w_concs[:TOP_N], scores_rel_s[:TOP_N])
+    bp.set_xticklabels(bp.get_xticklabels(), rotation=45, ha='right')
+    ylabel_name = 'Projection Proximity to \'Femininity\'' if isFemale else 'Projection Proximity to \'Masculinity\''
+    bp.set(ylabel=ylabel_name, xlabel='Words')
+    plt.tight_layout()
+    bp.get_figure().savefig(fn_name)
+    plt.show()
+
+def plot_pair_bias(model, female_list, male_list, feminine_coded_words_unstemmed, masculine_coded_words_unstemmed,
+                   TOP_N=25, comp = 1, fn_name="pair_bias.png"):
+    scores_rel = []
+    base_score = get_base_score(male_list, female_list)
     for w_f in feminine_coded_words_unstemmed:
         for w_m in masculine_coded_words_unstemmed:
             if w_f in model and w_m in model:
                 scores_rel.append(("{} vs. {}".format(w_f, w_m),
-                                   relative_gender_score(w_m, w_f, base_score, model, comp = 4)))
+                                   relative_gender_score(w_m, w_f, base_score, model, comp = comp)))
     scores_rel.sort(reverse=True, key=lambda x: x[1])
     w_concs = [w for w, s in scores_rel]
     scores_rel_s = [s for w, s in scores_rel]
-    pp.pprint(scores_rel[:5])
-    TOP_N = 25
-    plt.figure(figsize=(8, 4))
+    plt.figure(figsize=(14, 7))
     bp = sns.barplot(w_concs[:TOP_N], scores_rel_s[:TOP_N])
     bp.set_xticklabels(bp.get_xticklabels(), rotation=45, ha='right')
-    bp.set(ylabel='Proximity to Gender Bias', xlabel='F vs. M Word Pairs')
+    bp.set(ylabel='Projection Proximity to Gender Rift', xlabel='F vs. M Word Pairs')
     plt.tight_layout()
-    bp.get_figure().savefig('most_biased.png')
+    bp.get_figure().savefig(fn_name)
     plt.show()
 
+if __name__ == '__main__':
+    g_d = build_gender_direction(model, comp=1)
+    p1, p2 = "father", "doctor"
+    n1 = "mother"
+    print("{} is to {} as {} is to: ".format(p1, p2, n1))
+    # pp.pprint(model.most_similar(positive=[p1, n1], negative=[p2]))
+    base_score = get_base_score(male_list, female_list)
 
+    word_m_bias, word_f_bias = "doctor", "nurse"
+    score = absolute_gender_score(word_m_bias, word_f_bias, model, comp=1)[1]
+    print("Relative gender score of {} vs. {}: {} / {} = {}".format(word_m_bias, word_f_bias,
+                                                                    score, base_score,
+                                                                    relative_gender_score(word_m_bias, word_f_bias,
+                                                                                          base_score, model,
+                                                                                          comp=1)))
+    base_f_score = get_single_base_score(female_list, isFemale=True)
+    base_m_score = get_single_base_score(male_list, isFemale=False)
+    print("Gender score of {} is {} \n Gender score of {} is {}".format(word_m_bias,
+                                                                        relative_single_gender_score(word_m_bias,
+                                                                                                     base_m_score,
+                                                                                                     model, comp=1),
+                                                                        word_f_bias,
+                                                                        relative_single_gender_score(word_f_bias,
+                                                                                                     base_f_score,
+                                                                                                     model,
+                                                                                                     comp=1)))
+
+    # feminine_coded_words_unstemmed = ["prance", "retreat", "shy", "shriek", "pout", "embrace", "nag", "whine","snap"]
+    # masculine_coded_words_unstemmed = ["force", "pound", "stand", "hammer", "boom", "shout", "shoot"]
+    plot_pair_bias(model, female_list, male_list, feminine_coded_words_unstemmed, masculine_coded_words_unstemmed)
+    plot_single_bias(model, female_list, feminine_coded_words_unstemmed, fn_name='feminine_bias.png', isFemale=True)
+    plot_single_bias(model, male_list, masculine_coded_words_unstemmed, fn_name="masculine_bias.png", isFemale=False)
