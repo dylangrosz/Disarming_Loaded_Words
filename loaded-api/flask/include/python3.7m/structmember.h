7ec3873a3b08f0ff7f8221c41f50d4b289fc99b8
dylangrosz <dylangrosz@gmail.com>
2019-12-04 19:48:10 -0800

    add flask dependencies

--- loaded-api/flask/include/python3.7m/structmember.h
+++ loaded-api/flask/include/python3.7m/structmember.h
@@ -1,1 +1,1 @@
-/usr/local/Cellar/python/3.7.4_1/Frameworks/Python.framework/Versions/3.7/include/python3.7m/structmember.h
+C:/usr/local/Cellar/python/3.7.4_1/Frameworks/Python.framework/Versions/3.7/include/python3.7m/structmember.h
